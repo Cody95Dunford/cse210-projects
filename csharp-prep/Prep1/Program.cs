da33867ef1a919ede6255a79f0f4d24664586afc
Cody95Dunford <cddunford@byui.edu>
2024-09-16 14:22:36 -0500

    added comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // ask first name
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
 
+        // ask last name
         Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
+
+        // finished line
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
