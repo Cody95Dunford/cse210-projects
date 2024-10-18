0c102898783d7da9f89ef6cd7176b571908f3201
Cody95Dunford <cddunford@byui.edu>
2024-10-17 20:19:09 -0500

    finish learning assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        /*Step 3: Create the base class*/
+        Assignment assignment = new Assignment("Samual Bennett", "Multiplication");
+
+        /*Step 4: Create the mathAssignment class*/
+        assignment.GetSummary();        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        /*Step 5: Create the writingAssignment class*/
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
